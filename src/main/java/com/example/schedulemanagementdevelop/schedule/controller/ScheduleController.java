8a348c62b2544b3cf4c2b7558ee8420712c7c635
sirisiyesiri <shil690321@naver.com>
2026-04-16 19:19:25 +0900

    feat: 일정 생성 기능 구현

--- src/main/java/com/example/schedulemanagementdevelop/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanagementdevelop/schedule/controller/ScheduleController.java
@@ -1,9 +1,13 @@
 package com.example.schedulemanagementdevelop.schedule.controller;
 
+import com.example.schedulemanagementdevelop.schedule.dto.CreateScheduleRequest;
+import com.example.schedulemanagementdevelop.schedule.dto.CreateScheduleResponse;
 import com.example.schedulemanagementdevelop.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +17,7 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping("/schedules")
-    public ResponseEntity<>
+    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
+    }
 }
--- src/main/java/com/example/schedulemanagementdevelop/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedulemanagementdevelop/schedule/service/ScheduleService.java
@@ -1,12 +1,36 @@
 package com.example.schedulemanagementdevelop.schedule.service;
 
+import com.example.schedulemanagementdevelop.schedule.dto.CreateScheduleRequest;
+import com.example.schedulemanagementdevelop.schedule.dto.CreateScheduleResponse;
+import com.example.schedulemanagementdevelop.schedule.entity.Schedule;
 import com.example.schedulemanagementdevelop.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+
+    @Transactional
+    public CreateScheduleResponse save(CreateScheduleRequest request) {
+        Schedule schedule = new Schedule(
+                request.getTitle(),
+                request.getContent(),
+                request.getAuthorName(),
+                request.getPassword()
+        );
+
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new CreateScheduleResponse(
+                savedSchedule.getId(),
+                savedSchedule.getTitle(),
+                savedSchedule.getContent(),
+                savedSchedule.getAuthorName(),
+                savedSchedule.getCreatedAt(),
+                savedSchedule.getModifiedAt()
+        );
+    }
 }
