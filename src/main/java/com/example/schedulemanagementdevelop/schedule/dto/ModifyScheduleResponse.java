a62edfef2fc13dc13fab2d34f843adf640f2f8b1
sirisiyesiri <shil690321@naver.com>
2026-04-16 20:07:02 +0900

    feat: ModifyScheduleResponse(dto) 생성

---
+++ src/main/java/com/example/schedulemanagementdevelop/schedule/dto/ModifyScheduleResponse.java
@@ -0,0 +1,18 @@
+package com.example.schedulemanagementdevelop.schedule.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+public class ModifyScheduleResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String authorName;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+}
