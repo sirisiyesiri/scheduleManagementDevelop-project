f4c21d61b09f6d6393b93707b50cbdc50fca850d
sirisiyesiri <shil690321@naver.com>
2026-04-16 19:24:55 +0900

    feat: GetOneScheduleResponse(dto) 생성

--- src/main/java/com/example/schedulemanagementdevelop/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanagementdevelop/schedule/controller/ScheduleController.java
@@ -6,6 +6,7 @@ import com.example.schedulemanagementdevelop.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +21,7 @@ public class ScheduleController {
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
     }
+
+    @GetMapping("/schedules/{scheduleId}")
+    public ResponseEntity<>
 }
