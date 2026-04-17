7ba9ed194dba5e1d21ffc0f1dc9e4600e5aa0717
sirisiyesiri <shil690321@naver.com>
2026-04-17 14:14:00 +0900

    feat: User 추가 기능 구현

--- src/main/java/com/example/schedulemanagementdevelop/user/controller/UserController.java
+++ src/main/java/com/example/schedulemanagementdevelop/user/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.schedulemanagementdevelop.user.controller;
 
+import com.example.schedulemanagementdevelop.user.dto.CreateUserRequest;
+import com.example.schedulemanagementdevelop.user.dto.CreateUserResponse;
 import com.example.schedulemanagementdevelop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<>
+    public ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
+    }
 }
--- src/main/java/com/example/schedulemanagementdevelop/user/service/UserService.java
+++ src/main/java/com/example/schedulemanagementdevelop/user/service/UserService.java
@@ -1,13 +1,35 @@
 package com.example.schedulemanagementdevelop.user.service;
 
+import com.example.schedulemanagementdevelop.user.dto.CreateUserRequest;
+import com.example.schedulemanagementdevelop.user.dto.CreateUserResponse;
+import com.example.schedulemanagementdevelop.user.entity.User;
 import com.example.schedulemanagementdevelop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public CreateUserResponse save(CreateUserRequest request) {
+        User user = new User(
+                request.getUserName(),
+                request.getEmail()
+        );
+
+        User savedUser = userRepository.save(user);
+
+        return new CreateUserResponse(
+                savedUser.getId(),
+                savedUser.getUserName(),
+                savedUser.getEmail(),
+                savedUser.getCreatedAt(),
+                savedUser.getModifiedAt()
+        );
+    }
 }
