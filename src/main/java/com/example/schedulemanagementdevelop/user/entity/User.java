3d9c321ac222cf1c62679244246576c5d2473ec0
sirisiyesiri <shil690321@naver.com>
2026-04-17 11:13:51 +0900

    feat: User 엔티티 생성

---
+++ src/main/java/com/example/schedulemanagementdevelop/user/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.schedulemanagementdevelop.user.entity;
+
+import com.example.schedulemanagementdevelop.schedule.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String email;
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+}
