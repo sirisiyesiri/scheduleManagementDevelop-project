10200486d7da24afaf3dcff563196b866501988f
sirisiyesiri <shil690321@naver.com>
2026-04-17 14:16:12 +0900

    feat: GetOneUserResponse(dto) 생성

--- src/main/java/com/example/schedulemanagementdevelop/user/controller/UserController.java
+++ src/main/java/com/example/schedulemanagementdevelop/user/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +22,7 @@ public class UserController {
     public ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
     }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<>
 }
